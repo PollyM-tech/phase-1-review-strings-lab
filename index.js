0756c6964125d21d141d5349b059d1cb17f16d19
Pauline Moraa <pmoraa431@gmail.com>
2025-03-06 00:07:10 +0300

    Concatenate strings with the + operator Interpolate variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Polly Moraa';
+//including Polly Moraa to welcome message using interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
